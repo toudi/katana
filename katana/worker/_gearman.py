8c5b54384c2f30a5c6cb34a5ea37ccd1e0bc3cde
Mateusz Mikołajczyk <mikolajczyk.mateusz@gmail.com>
2014-04-16 15:55:33 +0200

    removed debug info

--- katana/worker/_gearman.py
+++ katana/worker/_gearman.py
@@ -9,10 +9,8 @@ class Client(object):
         self.client = GearmanClient(
             self.config.get('gearman', 'hosts').split(',')
         )
-        print(self.client)
 
     def process_transaction(self, transaction_id, background=True):
-        print('got to submit a job', transaction_id, background)
         self.client.submit_job(
             self.config.get('gearman', 'taskname'),
             str(transaction_id),
@@ -29,10 +27,8 @@ class Worker(AbstractWorker):
         self.worker.register_task(self.config.get('gearman', 'taskname'), self.process)
 
     def process(self, worker, job):
-        print('got a job to do', job)
         self.process_transaction(unicode(job.data))
         return 'ok'
 
     def run(self):
-        print('starting to work')
         self.worker.work()
